89c2f1cfd80d041ab779022f8dcf7ec976702f56
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2022-07-21 05:01:20 -0700

    Upgrade GetRaces() to use concurrency
    This is a 7x increase in speed (~700ms to ~100ms)

--- data/races.go
+++ data/races.go
@@ -4,6 +4,8 @@ import (
 	"encoding/xml"
 	"io/ioutil"
 	"net/http"
+	"sync"
+    "sort"
 )
 
 // Data source
@@ -30,50 +32,63 @@ type Races struct {
 	} `xml:"race"`
 }
 
+// Concurrently retrieve races from data source
 func GetRaces() ([]string, error) {
-	raceFiles := []string{
+	raceFiles := [15]string{
 		"/PlayersHandbook/races-phb.xml",
-		//"/DungeonMastersGuide/races-dmg.xml",
-		//"/PrincesOfTheApocalypse/races-eepc.xml",
-		//"/MordenkainensTomeOfFoes/races-mtf.xml",
-		//"/VolosGuideToMonsters/races-vgm.xml",
-		//"/GuildmastersGuideToRavnica/races-ggr.xml",
-		//"/EberronRisingFromTheLastWar/races-erlw.xml",
-		//"/AcquisitionsIncorporated/races-ai.xml",
-		//"/ExplorersGuideToWildemount/races-egw.xml",
-		//"/TheTortlePackage/races-ttp.xml",
-		//"/OneGrungAbove/races-oga.xml",
-		//"/MythicOdysseysOfTheros/races-mot.xml",
-		//"/TashasCauldronOfEverything/races-tce.xml",
-		//"/LocathahRising/races-lr.xml",
-		//"/AdventureWithMuk/races-awm.xml",
+		"/DungeonMastersGuide/races-dmg.xml",
+		"/PrincesOfTheApocalypse/races-eepc.xml",
+		"/MordenkainensTomeOfFoes/races-mtf.xml",
+		"/VolosGuideToMonsters/races-vgm.xml",
+		"/GuildmastersGuideToRavnica/races-ggr.xml",
+		"/EberronRisingFromTheLastWar/races-erlw.xml",
+		"/AcquisitionsIncorporated/races-ai.xml",
+		"/ExplorersGuideToWildemount/races-egw.xml",
+		"/TheTortlePackage/races-ttp.xml",
+		"/OneGrungAbove/races-oga.xml",
+		"/MythicOdysseysOfTheros/races-mot.xml",
+		"/TashasCauldronOfEverything/races-tce.xml",
+		"/LocathahRising/races-lr.xml",
+		"/AdventureWithMuk/races-awm.xml",
 	}
 
 	var races []string
+	var wg sync.WaitGroup
+	var mt sync.Mutex
 	for _, raceFile := range raceFiles {
-		// Read data source
-		resp, err := http.Get(raceBaseURL + raceFile)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
+		go func(raceFile string, wg *sync.WaitGroup) ([]string, error) {
+			wg.Add(1)
+			defer wg.Done()
+			// Read data source
+			resp, err := http.Get(raceBaseURL + raceFile)
+			if err != nil {
+				return nil, err
+			}
+			defer resp.Body.Close()
 
-		// Convert to bytes
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return nil, err
-		}
+			// Convert to bytes
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return nil, err
+			}
 
-		// Unmarshal bytes to struct
-		var r Races
-		if err := xml.Unmarshal(body, &r); err != nil {
-			return nil, err
-		}
+			// Unmarshal bytes to struct
+			var r Races
+			if err := xml.Unmarshal(body, &r); err != nil {
+				return nil, err
+			}
 
-		for _, race := range r.Race {
-			races = append(races, race.Name)
-		}
+			mt.Lock()
+			for _, race := range r.Race {
+				races = append(races, race.Name)
+			}
+			mt.Unlock()
+
+			return races, nil
+		}(raceFile, &wg)
 	}
+	wg.Wait()
 
+    sort.Strings(races)
 	return races, nil
 }
