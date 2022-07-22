9722fea242a5b7eadddaaf0647ef61006c14f203
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2022-07-21 22:25:33 -0700

    Set up code & pipeline for semantic versioning

--- cmd/root.go
+++ cmd/root.go
@@ -9,10 +9,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var Version = "development"
+
 var rootCmd = &cobra.Command{
-	Use:   "dndcc",
-	Short: "A DnD 5e Character Creator",
-	Long:  "dndcc is a 5th Edition Dungeons & Dragons Character Creator",
+	Use:     "dndcc",
+	Short:   "A DnD 5e Character Creator",
+	Long:    "dndcc is a 5th Edition Dungeons & Dragons Character Creator",
+	Version: Version, // Overridden by ldflags at build time
 	RunE: func(cmd *cobra.Command, args []string) error {
 		app := tea.NewProgram(ui.InitialModel())
 		if err := app.Start(); err != nil {
@@ -20,6 +23,10 @@ var rootCmd = &cobra.Command{
 		}
 		return nil
 	},
+}
+
+func init() {
+	rootCmd.SetVersionTemplate("{{.Version}}\n")
 }
 
 func Execute() {
