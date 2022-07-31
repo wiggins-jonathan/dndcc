f6bef5ff401e46c70910edad0d582fcc5d6260b2
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2022-07-31 00:08:57 -0700

    Move itemDelegate to common.go

--- ui/common.go
+++ ui/common.go
@@ -1,8 +1,12 @@
 package ui
 
 import (
+	"fmt"
+	"io"
+
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 type commonModel struct {
@@ -27,4 +31,28 @@ func newCommonModel() *commonModel {
 	}
 
 	return &commonModel{list: l}
+}
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                               { return 1 }
+func (d itemDelegate) Spacing() int                              { return 0 }
+func (d itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
+
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%s", i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s string) string {
+			return selectedItemStyle.Render(s)
+		}
+	}
+
+	fmt.Fprintf(w, fn(str))
 }
